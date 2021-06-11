0dfb46f1a896dff193cd8d81efea91763842cc7c
Tien Pham <tien@phaw.io>
2021-06-11 14:28:21 +0700

    Update example project

--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,92 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import MaskedView from '@react-native-community/masked-view'
 import { SquircleView } from 'react-native-figma-squircle'
+import RNSlider, {
+  SliderProps as RNSliderProps,
+} from '@react-native-community/slider'
 
 export default function App() {
+  const [cornerRadius, setCornerRadius] = useState(30)
+  const [cornerSmoothing, setCornerSmoothing] = useState(0.8)
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <SquircleView
-        cornerRadius={30}
-        cornerSmoothing={0.8}
-        style={{ width: 200, height: 200 }}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <StatusBar style="light" />
+
+        <View style={styles.sliders}>
+          <Slider
+            label="Corner radius"
+            minimumValue={20}
+            maximumValue={100}
+            step={1}
+            value={cornerRadius}
+            onValueChange={setCornerRadius}
+          />
+
+          <View style={{ height: 24 }} />
+          <Slider
+            label="Corner smoothing"
+            minimumValue={0}
+            maximumValue={1}
+            step={0.01}
+            value={cornerSmoothing}
+            onValueChange={(value) => {
+              setCornerSmoothing(Math.round(value * 100) / 100)
+            }}
+          />
+        </View>
+        <View style={styles.squircles}>
+          <SquircleView
+            style={styles.squircle}
+            cornerRadius={cornerRadius}
+            cornerSmoothing={cornerSmoothing}
+            fillColor="#4F46E5"
+            strokeColor="#6366F1"
+            strokeWidth={4}
+          />
+
+          <MaskedView
+            maskElement={
+              <SquircleView
+                style={StyleSheet.absoluteFill}
+                cornerRadius={cornerRadius}
+                cornerSmoothing={cornerSmoothing}
+              />
+            }
+          >
+            <LinearGradient
+              style={styles.squircle}
+              start={{ x: 1, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              colors={['#FF1B6B', '#45CAFF']}
+            />
+          </MaskedView>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+interface SliderProps extends Omit<RNSliderProps, 'ref'> {
+  label: string
+}
+
+function Slider({ label, value, ...rest }: SliderProps) {
+  return (
+    <View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={styles.label}>{label}</Text>
+        <Text style={styles.sliderValue}>{value}</Text>
+      </View>
+      <RNSlider
+        value={value}
+        minimumTrackTintColor="#6e78ff"
+        maximumTrackTintColor="#737373"
+        {...rest}
       />
     </View>
   )
@@ -19,8 +95,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171717',
+  },
+  contentContainer: {
+    flex: 1,
+    backgroundColor: '#171717',
+    padding: 16,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#A3A3A3',
+  },
+  sliderValue: {
+    fontSize: 16,
+    color: '#E5E5E5',
+    fontWeight: 'bold',
+  },
+  squircles: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  squircle: {
+    width: '60%',
+    aspectRatio: 1,
+  },
+  sliders: {
+    paddingVertical: 28,
   },
 })
